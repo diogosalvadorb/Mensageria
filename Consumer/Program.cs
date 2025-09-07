4d1ea2c8ef3a020101d3025b08847398b789b151
diogosalvadorb <diogosalvadorb@gmail.com>
2025-09-07 20:05:09 -0300

    send object by message

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
+using System.Text.Json;
 
 var factory = new ConnectionFactory { HostName = "localhost" };
 
@@ -16,20 +17,38 @@ await channel.QueueDeclareAsync(
 
 Console.WriteLine("[x] Aguardando mensagens.");
 
+// Cria um consumidor assíncrono que ficará "escutando" mensagens
 var consumer = new AsyncEventingBasicConsumer(channel);
+
+//DELEGATE - uma funcao que é executada quando recebe um evento ReceivedAsync                       
 consumer.ReceivedAsync += (model, ea) =>
 {
+    //converte o corpo da mensagem para array de bytes
     var body = ea.Body.ToArray();
+
+    // Decodifica os bytes para string (UTF-8)
     var message = Encoding.UTF8.GetString(body);
 
+    // Desserializa a string JSON para um objeto Plane
+    var plane = JsonSerializer.Deserialize<Plane>(message);
+
     Console.WriteLine($" [x] Mensagem recebida = {message}");
+
+    // Retorna uma Task completada (padrão para métodos assíncronos de eventos)
     return Task.CompletedTask;
 };
 
+// Começa a consumir mensagens da fila "hello"
 await channel.BasicConsumeAsync(
-    queue: "hello",
-    autoAck: true,
-    consumer: consumer);
+    queue: "hello",// Nome da fila
+    autoAck: true,// true = confirma automaticamente o recebimento
+    consumer: consumer);// consumer criado acima
 
 Console.WriteLine("Pressione [enter] para sair.");
 Console.ReadLine();
+
+class Plane
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,11 +1,16 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
+using System.Text.Json;
 
+//cria uma conexão que se conectar ao RabbitMQ
 var factory = new ConnectionFactory() { HostName = "localhost" };
 
+//abre uma conexão assicrona com o servidor rabbitMQ(versão 7.1.2)
+//cria um canal de comunicação dentro da conexao (declarar, consumir, publicar fila) são operações que são feitas dentro do canal
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
+//declara uma fila chamada hello, se a fila já existente não faz nada
 await channel.QueueDeclareAsync(
             queue: "hello",
             durable: false,
@@ -18,13 +23,26 @@ Console.WriteLine("Digite uma mensagem e presione Enter");
 while (true)
 {
     var message = Console.ReadLine();
+
+    //declara um objeto e serializa para json
+    var plane = new Plane { Id = 1, Name = "Boeing 737" };
+    message = JsonSerializer.Serialize(plane);
+
     if (string.IsNullOrEmpty(message)) break;
+    //converte a mensagem(string) para um array de bytes, pois o RabbitMQ trabalha com array de bytes
     var body = Encoding.UTF8.GetBytes(message);
 
+    // Publica a mensagem na fila "hello"
     await channel.BasicPublishAsync(
-                exchange: string.Empty,
-                routingKey: "hello",
-                body: body);
+                exchange: string.Empty, //Usa o Default Exchange
+                routingKey: "hello", // Nome da fila onde vai ser publicada a mensagem
+                body: body); // Corpo da mensagem em bytes
 
     Console.WriteLine($" [x] Enviado {message}");
+}
+
+class Plane
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
