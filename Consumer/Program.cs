a361761812603b4eb52c18a3daf5217bb5e27113
diogosalvadorb <diogosalvadorb@gmail.com>
2025-09-07 16:18:21 -0300

    consumer message

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+            queue: "hello", 
+            durable: false, 
+            exclusive: false, 
+            autoDelete: false,
+            arguments: null);
+
+Console.WriteLine("[x] Aguardando mensagens.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($" [x] Mensagem recebida = {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine("Pressione [enter] para sair.");
+Console.ReadLine();
