d379aafd2d426f44149d89af0ab4dd2347455503
diogosalvadorb <diogosalvadorb@gmail.com>
2025-09-07 13:15:21 -0300

    Publisher

---
+++ Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+Console.WriteLine("Digite uma mensagem e presione Enter");
+
+while (true)
+{
+    var message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message)) break;
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: "hello",
+                body: body);
+
+    Console.WriteLine($" [x] Enviado {message}");
+}
